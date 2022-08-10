9bbc5da21374a5ac2e6624655dfd97345d2d3841
Matheus Palma <mmatheuspalma@gmail.com>
2022-08-10 14:03:41 -0300

    fix: removing the bridge instance from the default.vue file

--- layouts/default.vue
+++ layouts/default.vue
@@ -111,15 +111,6 @@ const cssVariables = computed(() => {
   }
   return theme
 })
-
-const { slug } = useRoute().params
-
-onMounted(() => {
-  if (slug && slug[0] !== 'site-config') return
-  useStoryblokBridge(story.value.id, (evStory) => (story.value = evStory), {
-    preventClicks: true, // useful here to prevent default behavior when clicking on buttons/links
-  })
-})
 </script>
 
 <style>
