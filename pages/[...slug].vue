ee626a78260832aa17d4f1db07292c8e72efd0bf
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-08-30 15:43:06 +0200

    remove customParent

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -55,8 +55,6 @@ story.value = data.story
 onMounted(() => {
   if (slug[0] !== undefined && slug[0] === 'site-config') return
   useStoryblokBridge(story.value.id, (evStory) => (story.value = evStory), {
-    customParent:
-      'https://storyfront-git-feat-activate-new-demo-storyblok-com.vercel.app/',
     resolveRelations: resolveRelations,
   })
 })
