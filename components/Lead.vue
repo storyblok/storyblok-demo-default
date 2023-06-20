2db69a1532c1bd8444460818191d34602a3eb146
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-06-20 10:29:02 +0200

    Lead text color

--- components/Lead.vue
+++ components/Lead.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="text-lg md:text-xl leading-relaxed md:leading-relaxed mb-6">
+  <div
+    class="text-lg md:text-xl leading-relaxed md:leading-relaxed mb-6 text-dark"
+  >
     <slot />
   </div>
 </template>
