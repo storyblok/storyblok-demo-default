1626023ac78aa35480bb115dc492290b2a7c5242
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-08-07 11:23:12 +0200

    update deps

--- storyblok/BannerReference.vue
+++ storyblok/BannerReference.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Navigation } from 'swiper'
+import { Navigation } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 const props = defineProps({ blok: Object })
