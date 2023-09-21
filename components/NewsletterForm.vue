1aa9c72b2c2a24303a622a65baa6471d8557966c
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-09-21 11:51:09 +0200

    update

--- components/NewsletterForm.vue
+++ components/NewsletterForm.vue
@@ -1,4 +1,6 @@
 <script setup>
+const props = defineProps({ textColor: String })
+
 const showMessage = ref(false)
 
 const submit = () => {
@@ -8,14 +10,21 @@ const submit = () => {
   }, 4000)
 }
 
-const button = {
-  link: {
-    linktype: 'url',
-  },
-  size: 'default',
-  style: 'default',
-  button_color: 'primary',
-}
+const borderColor = computed(() => {
+  return props.textColor === 'text-white' ? 'border-light' : 'border-dark'
+})
+
+const button = computed(() => {
+  return {
+    link: {
+      linktype: 'url',
+    },
+    size: 'default',
+    style: 'default',
+    text_color: props.textColor === 'text-white' ? 'dark' : 'light',
+    background_color: props.textColor === 'text-white' ? 'light' : 'dark',
+  }
+})
 </script>
 
 <style scoped>
@@ -34,44 +43,29 @@ const button = {
   <form action="" class="relative">
     <div class="flex flex-col space-y-6">
       <div
-        class="flex items-center justify-center space-x-1 text-white lg:justify-start"
+        class="flex items-center justify-center space-x-1 lg:justify-start"
+        :class="textColor"
       >
         <h4 class="text-xl">Sign up for our newsletter</h4>
       </div>
       <input
         type="email"
         placeholder="Your email"
-        class="rounded-lg border border-white bg-transparent px-6 py-4 text-white transition-all focus:outline-none"
+        class="rounded-lg border bg-transparent px-6 py-4 transition-all focus:outline-none"
+        :class="[textColor, borderColor]"
         required
       />
     </div>
 
     <div class="flex justify-center lg:justify-start">
-      <Button :button="button" class="mt-8" @click="submit">
-        <div class="flex items-center space-x-2">
-          <span>Sign up</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-            />
-          </svg>
-        </div>
-      </Button>
+      <Button :button="button" class="mt-8" @click="submit">Sign up</Button>
     </div>
 
     <Transition name="fade">
       <div
         v-if="showMessage"
-        class="botttom-0 absolute left-0 translate-y-full text-lg text-white"
+        class="botttom-0 absolute left-0 translate-y-full text-lg"
+        :class="textColor"
       >
         Thank you!
       </div>
--- storyblok/FormSection.vue
+++ storyblok/FormSection.vue
@@ -4,6 +4,14 @@ import NewsletterForm from '../components/NewsletterForm.vue'
 
 const props = defineProps({ blok: Object })
 
+const textColor = computed(() => {
+  return props.blok.text_color === 'light' ? 'text-white' : 'text-dark'
+})
+
+const backgroundColor = computed(() => {
+  return 'bg-' + props.blok.background_color
+})
+
 const selectedForm = computed(() => {
   switch (props.blok.form) {
     case 'newsletter':
@@ -16,18 +24,26 @@ const selectedForm = computed(() => {
 </script>
 
 <template>
-  <section v-editable="blok" class="page-section form-section relative bg-dark">
+  <section
+    v-editable="blok"
+    class="page-section form-section relative"
+    :class="backgroundColor"
+  >
     <div
       class="container relative z-10 grid place-items-center gap-6 sm:gap-10 md:gap-12 lg:grid-cols-2"
     >
       <div class="text-center text-white lg:text-left">
-        <Headline v-if="blok.headline" class="mb-4" color="white">
+        <Headline
+          v-if="blok.headline"
+          class="mb-4"
+          :color="blok.text_color === 'light' ? 'white' : 'dark'"
+        >
           {{ blok.headline }}
         </Headline>
-        <Lead v-if="blok.lead">{{ blok.lead }}</Lead>
+        <Lead v-if="blok.lead" :class="textColor">{{ blok.lead }}</Lead>
       </div>
       <div class="w-full max-w-md">
-        <component :is="selectedForm" />
+        <component :is="selectedForm" :textColor="textColor" />
       </div>
     </div>
   </section>
